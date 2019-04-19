1c88e767064e4d6298bea7d7fbd82be4471b6501
flodi <florent.dinet@lumapps.com>
2019-04-19 17:25:13 +0200

    docs(link): added page + doc
    added link doc page in app and added text + layout doc

--- demo/angularjs/app.js
+++ demo/angularjs/app.js
@@ -144,6 +144,16 @@ function AppDefaultConfig($locationProvider, $stateProvider, markedProvider) {
                 },
             },
         })
+        .state('app.link', {
+            url: 'link',
+            views: {
+                'main@': {
+                    controller: 'DemoLinkController',
+                    controllerAs: 'vm',
+                    template: require('./components/link/demo.html'),
+                },
+            },
+        })
         .state('app.list', {
             url: 'list',
             views: {
@@ -342,6 +352,7 @@ require('./components/dialog/controller.js');
 require('./components/dropdown/controller.js');
 require('./components/expansion-panel/controller.js');
 require('./components/lightbox/controller.js');
+require('./components/link/controller.js');
 require('./components/list/controller.js');
 require('./components/notification/controller.js');
 require('./components/progress-tracker/controller.js');
